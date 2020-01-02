6d39ad407691c2f372f7c627ac74c41fcc4a6014
joris.monnet <joris.monnet@he-arc.ch>
2020-01-02 17:11:56 +0100

    no remember

--- enemy_moove_p2/gameview.cpp
+++ enemy_moove_p2/gameview.cpp
@@ -7,18 +7,18 @@ GameView::GameView(QWidget*parent,Map*map) : QWidget(parent)
     setMouseTracking(true);
     mainLay->setContentsMargins(0,0,0,0);
     mainLay->setSpacing(0);
+    setLayout(mainLay);
+
     if(map!=nullptr){
         this->map=new Map(nullptr,map->path,map->towerNumber,map->towerPositions,map->money,new QGraphicsPixmapItem(map->background->pixmap()));
         mainLay->addWidget(map);
         towerMenu= new TowerMenu(nullptr,map->towerClicked);
-
+        connect(towerMenu,&TowerMenu::finished,this,[=]{mainLay->removeWidget(towerMenu);});
         connect(map,&Map::clickedTower,[=]{
             if(mainLay->count()<2){
                 towerMenu= new TowerMenu(nullptr,map->towerClicked);
                 mainLay->addWidget(towerMenu);
             }
         });
-        connect(towerMenu,&TowerMenu::finished,this,[=]{mainLay->removeWidget(towerMenu);});
     }
-    setLayout(mainLay);
 }
