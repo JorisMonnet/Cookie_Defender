c010b337bbab3c98c11dcefbc04e4589ee8bebac
adrien.paysant <adrien.paysant@he-arc.ch>
2020-01-07 11:04:04 +0100

    reducing monster speed and life

--- cookie_defender/monster.h
+++ cookie_defender/monster.h
@@ -8,10 +8,10 @@ class Monster : public QGraphicsPixmapItem
 public:
     Monster();
     const int size=40;
-    int hp=50;
+    int hp=20;
     int shield=25;
     int reward=25;
-    const int velocity=3;
+    const int velocity=2;
     const int dammage=1;
     int pathIndex=0;
     void move(QVector<QPointF>path);
