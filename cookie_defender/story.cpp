96d335af6832cce2878d68f465d234ff0bff78da
adrien.paysant <adrien.paysant@he-arc.ch>
2020-01-09 17:33:13 +0100

    adjusting policy and size for story

--- cookie_defender/story.cpp
+++ cookie_defender/story.cpp
@@ -8,15 +8,16 @@ Story::Story(QWidget *parent) : QWidget(parent)
 {
     QLabel *lab= new QLabel();
     lab->setTextFormat(Qt::AutoText);
-    lab->setText("Depuis des temps immémoriaux, dans le jovial village de Sevenans, \nla famille Cook chérie et conserve précieusement "
+    lab->setText(tr("Depuis des temps immémoriaux, dans le jovial village de Sevenans, \nla famille Cook chérie et conserve précieusement "
                  "la recette du sacro-saint cookie : le cookie de tatie Georgette.\nCe cookie si tendre est fondant mais"
                  " en même temps croustillant et mielleux a\n par le passé déclenché moultes révoltes lors du quatre heure"
                  " journalier des écoles de toute la région. \n"
                  "Aujourd’hui la guerre continue ! \nLevez-vous et protégez le cookie dussiez-vous y laisser votre vie !\n\n"
-                 "Soldat ! Le cookie a besoin de vous !");
+                 "Soldat ! Le cookie a besoin de vous !"));
+    lab->setAlignment(Qt::AlignCenter);
+    lab->setFont(QFont("Brush Script MT",25));
 
-
-    QPushButton *exit = new QPushButton(tr("exit"));
+    QPushButton *exit = new QPushButton(tr("&Exit"));
     QVBoxLayout *vLay = new QVBoxLayout();
     vLay->addStretch();
     vLay->addWidget(lab);
