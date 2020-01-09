06b6a5e44c0eea754b67b1e9b15a21a4760fa44b
adrien.paysant <adrien.paysant@he-arc.ch>
2020-01-09 17:31:06 +0100

    adding story

--- cookie_defender/story.cpp
+++ cookie_defender/story.cpp
@@ -1,11 +1,25 @@
 #include "story.h"
 #include <QLayout>
+#include <QDebug>
+#include <QFile>
+#include <QLabel>
 
 Story::Story(QWidget *parent) : QWidget(parent)
 {
+    QLabel *lab= new QLabel();
+    lab->setTextFormat(Qt::AutoText);
+    lab->setText("Depuis des temps immémoriaux, dans le jovial village de Sevenans, \nla famille Cook chérie et conserve précieusement "
+                 "la recette du sacro-saint cookie : le cookie de tatie Georgette.\nCe cookie si tendre est fondant mais"
+                 " en même temps croustillant et mielleux a\n par le passé déclenché moultes révoltes lors du quatre heure"
+                 " journalier des écoles de toute la région. \n"
+                 "Aujourd’hui la guerre continue ! \nLevez-vous et protégez le cookie dussiez-vous y laisser votre vie !\n\n"
+                 "Soldat ! Le cookie a besoin de vous !");
+
 
     QPushButton *exit = new QPushButton(tr("exit"));
     QVBoxLayout *vLay = new QVBoxLayout();
+    vLay->addStretch();
+    vLay->addWidget(lab);
     vLay->addStretch();
     vLay->addWidget(exit);
     vLay->addStretch();
