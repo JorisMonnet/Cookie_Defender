39441beb186bbab639ced4be90d0aab6b3fc9a75
adrien.paysant <adrien.paysant@he-arc.ch>
2020-01-09 18:26:42 +0100

    translating the cookie's story

--- cookie_defender/story.cpp
+++ cookie_defender/story.cpp
@@ -8,12 +8,14 @@ Story::Story(QWidget *parent) : QWidget(parent)
 {
     QLabel *lab= new QLabel();
     lab->setTextFormat(Qt::AutoText);
-    lab->setText(tr("Depuis des temps immémoriaux, dans le jovial village de Sevenans, \nla famille Cook chérie et conserve précieusement "
-                 "la recette du sacro-saint cookie : le cookie de tatie Georgette.\nCe cookie si tendre est fondant mais"
-                 " en même temps croustillant et mielleux a\n par le passé déclenché moultes révoltes lors du quatre heure"
-                 " journalier des écoles de toute la région. \n"
-                 "Aujourd’hui la guerre continue ! \nLevez-vous et protégez le cookie dussiez-vous y laisser votre vie !\n\n"
-                 "Soldat ! Le cookie a besoin de vous !"));
+    lab->setText(tr("For thousands of centuries, in the small funny village of Sevenans,\n"
+                    "the Cook family hide and protect the sacred cookie's recipe of aunty Georgia.\n"
+                    "This cookie is full of flavor and crispy but also with a bit of honey....\n"
+                    "Hum this cookie is damn perfect !\n"
+                    "By the past and currently the Cookie's War tear all the Sevenans's scholar on break time.\n\n"
+                    "We need you ! The cookie needs you !\n"
+                    "The war never stops ! \n"
+                    "Go on soldier ! "));
     lab->setAlignment(Qt::AlignCenter);
     lab->setFont(QFont("Brush Script MT",25));
 
