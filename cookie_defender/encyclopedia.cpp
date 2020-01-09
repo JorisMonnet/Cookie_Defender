36a24afce96f2dfd59425b3dc65edc6fd0db9453
adrien.paysant <adrien.paysant@he-arc.ch>
2020-01-09 18:14:29 +0100

    updating encyclopedia

--- cookie_defender/encyclopedia.cpp
+++ cookie_defender/encyclopedia.cpp
@@ -7,12 +7,15 @@
 
 Encyclopedia::Encyclopedia(QWidget *parent) : QWidget(parent)
 {
+
+
     Tower *mageTower = new Tower();
     mageTower->type=2;
     mageTower->set(1);
     Tower *classicTower = new Tower();
 
     Monster *monster = new Monster('A');
+    Monster *monster2 =new Monster('B');
 
     QGridLayout * grid = new QGridLayout(this);
 
@@ -37,12 +40,26 @@ Encyclopedia::Encyclopedia(QWidget *parent) : QWidget(parent)
     grid->addWidget(monsterImage,1,6);
 
     QLabel *monsterText = new QLabel;
-    monsterText->setText(QString(tr("Monster 1 \nDamage : %1\nVelocity : %2\nHealth : %3")).arg(monster->dammage).arg(monster->velocity).arg(monster->hp));
+    monsterText->setText(QString(tr("Zombie \nDamage : %1\nVelocity : %2\nHealth : %3")).arg(monster->dammage).arg(monster->velocity).arg(monster->hp));
     grid->addWidget(monsterText,2,6);
+
+    QLabel *monster2Image = new QLabel;
+    monster2Image->setPixmap(monster2->pixmap());
+    grid->addWidget(monster2Image,1,7);
+
+    QLabel *monster2Text = new QLabel;
+    monster2Text->setText(QString(tr("Troll  \nDamage : %1\nVelocity : %2\nHealth : %3")).arg(monster2->dammage).arg(monster2->velocity).arg(monster2->hp));
+    grid->addWidget(monster2Text,2,7);
+
+    classicTowerText->setFont(QFont("Brush Script MT",15));
+    mageTowerText->setFont(QFont("Brush Script MT",15));
+    monsterText->setFont(QFont("Brush Script MT",15));
+    monster2Text->setFont(QFont("Brush Script MT",15));
+
 
     QPushButton *exit = new QPushButton(tr("&Exit"));
     connect(exit,&QPushButton::clicked,this,[=]{emit finishEncyclo();});
-    grid->addWidget(exit,6,2);
+    grid->addWidget(exit,6,3);
     //to do create label with tower and monsters attributes
 
 }
